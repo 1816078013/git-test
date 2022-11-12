99e0ca4c290ea90d64c3812b1a99cb719ffd2512
TravelChanWeiZi <101806683+1816078013@users.noreply.github.com>
2022-11-13 06:12:26 +0800

    Update GitTest.java

--- src/main/java/com/xuexi/GitTest.java
+++ src/main/java/com/xuexi/GitTest.java
@@ -11,7 +11,7 @@ public class GitTest {
             System.out.println("hot-fix     test.........");
             System.out.println("push     test.........");
             System.out.println("pull     test.........");
-            System.out.println("push     test2.........");
+            System.out.println("push     test2.........111111111");
         }
     }
 
