ede4c9905fe51119df55f897e50744287da207f8
RoxTigerPeanut <1816078013@qq.com>
2022-11-13 06:02:03 +0800

    一次提交

---
+++ src/main/java/com/xuexi/GitTest.java
@@ -0,0 +1,18 @@
+package com.xuexi;
+
+public class GitTest {
+
+        public static void main(String[] args) {
+            System.out.println("Git  在  idea  环境当中的测试.....");
+            System.out.println("Hello  Git");
+            System.out.println("gogogo......");
+            System.out.println(" let's  go   we    together!!!");
+            System.out.println("master  test........");
+            System.out.println("hot-fix     test.........");
+            System.out.println("push     test.........");
+            System.out.println("pull     test.........");
+            System.out.println("push     test2.........");
+        }
+    }
+
+
